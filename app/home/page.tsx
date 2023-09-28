5aa706b3aa98050d5ffaa94f1a0e6a97235c7559
lokytech5 <lokytech5@gmail.com>
2023-09-28 15:06:49 +0100

    Added Hero section design to homepage

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import houseImg from '../../public/images/house1.jpg'
 
 const HomePage = () => {
   return (
-    <div className="bg-primary">Hello welcome to geoHomeFinder</div>
+    <div className="hero min-h-screen" style={{backgroundImage: `url('/images/house1.jpg')`}}>
+  <div className="hero-overlay bg-opacity-40"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+    <h1 className="mb-5 text-5xl font-bold">Discover Your Dream Home</h1>
+<p className="mb-5">GeoHomeFinder makes it easy for you and your loved ones.</p>
+
+
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
   )
 }
 
