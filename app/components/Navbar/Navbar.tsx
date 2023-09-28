9eede07bb2e4d305dbba0f32b34fa00877674757
lokytech5 <lokytech5@gmail.com>
2023-09-28 14:26:58 +0100

    Added Navigationbar

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import HamburgerMenu from './HamburgerMenu'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <div className='drawer'> 
+    <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
+    <div className="drawer-content flex flex-col">
+      <div className="navbar bg-base-100 text-secondary-content">
+      <div className="flex-none lg:hidden">
+        <HamburgerMenu/>
+    </div>
+    <div className="flex-1">
+      <Link href="/home" className="btn btn-ghost normal-case text-xl">GeoHomeFinder</Link>
+    </div>
+         
+         {/* <ThemeToggle/>
+         <DesktopMenu/> */}
+      <div className="flex-none">
+          {/* <Cart/>
+          <UserMenu/> */}
+      </div>
+    </div>
+    </div>
+    
+    {/* <Drawer/> */}
+  
+    </div>
+  )
+}
+
+export default Navbar
---
+++ app/home/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Hello welcome to geoHomeFinder</div>
+  )
+}
+
+export default page
