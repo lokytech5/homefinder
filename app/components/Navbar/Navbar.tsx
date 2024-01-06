b59e0d17f7309ebf0e1816a38edd32b97b50da8d
lokytech5 <lokytech5@gmail.com>
2024-01-06 08:33:52 +0100

    Added toggole button for theme to navigation menu

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -4,14 +4,18 @@ import HamburgerMenu from './HamburgerMenu'
 import Link from 'next/link'
 import DesktopMenu from './DesktopMenu'
 import Drawer from './Drawer'
+import useUserStore from '../store/useUserStore'
+import UserMenu from './UserMenu'
+import ThemeToggle from './ThemeToggle'
 
 const Navbar = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const isAuthenticated = useUserStore((state) => state.isAuthenticated);
   return (
     <div className='drawer'> 
     <input id="my-drawer-3" type="checkbox" className="drawer-toggle" checked={isDrawerOpen} onChange={() => setIsDrawerOpen(!isDrawerOpen)} />
     <div className="drawer-content flex flex-col">
-      <div className="navbar bg-neutral text-primary-content">
+      <div className="navbar bg-primary text-primary-content">
       <div className="flex-none lg:hidden">
         <HamburgerMenu/>
     </div>
@@ -19,11 +23,10 @@ const Navbar = () => {
       <Link href="/home" className="btn btn-ghost normal-case text-xl">GeoHomeFinder</Link>
     </div>
          
-          {/* <ThemeToggle/> */}
+          <ThemeToggle/>
          <DesktopMenu/> 
       <div className="flex-none">
-          {/* <Cart/>
-          <UserMenu/> */}
+      {isAuthenticated && <UserMenu />}
       </div>
     </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["lemonade"],
+    themes: ["bumblebee", "lemonade"],
   },
 }
 export default config
